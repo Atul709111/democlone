4e20a28bea68c6025a8bad631000180dff3ae335
Atul709111 <22je0201@iitism.ac.in>
2024-06-04 13:24:11 +0530

    try copy

--- README.md
+++
@@ -1,2 +0,0 @@
-# democlone
-i dont know
--- js/index.js
+++
@@ -1,6 +0,0 @@
-let menuHamburger = document.querySelector('.menu__hamburger');
-let mobileMenu = document.querySelector('.mobile-menu');
-
-menuHamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-})
